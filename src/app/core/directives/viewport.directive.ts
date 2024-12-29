129951d0bb5db4afec52cd6ef0bfe0e09809c812
geff <geffyjebasekar@gmail.com>
2024-12-29 20:21:56 +0530

    scroll fix using style.scss and latest code changes

--- src/app/core/directives/viewport.directive.ts
+++ src/app/core/directives/viewport.directive.ts
@@ -1,11 +1,31 @@
-import { Directive, ElementRef, signal } from '@angular/core';
+import { Directive, ElementRef, OnDestroy, OnInit, output, signal } from '@angular/core';
+import { PortfolioCommonService } from '../services/portfolio-common.service';
 
 @Directive({
   selector: '[appViewport]'
 })
-export class ViewportDirective {
+export class ViewportDirective implements OnInit,OnDestroy {
 
-  viewportSignal = signal('');
-  constructor(private el : ElementRef) { }
+  isViewport = output<boolean>();
+  private observer !: IntersectionObserver;
+  constructor(private el : ElementRef, private portfolioCommonService : PortfolioCommonService) { }
+  ngOnInit(): void {
+    this.observer = new IntersectionObserver(([entry]) => {
+      this.portfolioCommonService.viewportSignal.set('home');
+      this.isViewport.emit(entry.isIntersecting);
+    },
+    {
+      root: null, // The viewport,
+      rootMargin: '0px', //Margin around the root,
+      threshold: 0.0 // the directive to act if any part of the element is visible
+    }
+  );
+  this.observer.observe(this.el.nativeElement);
+  }
+
+  ngOnDestroy(): void {
+    // this.observer.unobserve(this.el.nativeElement);
+    this.observer.disconnect();
+  }
 
 }
